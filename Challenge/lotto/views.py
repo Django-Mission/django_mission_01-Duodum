40bab8a4d7df664e14d06aa695f1b1da97156154
A\94323 <94323487@naver.com>
2022-03-31 12:21:29 +0900

    # Update Challenge

--- Challenge/lotto/views.py
+++ Challenge/lotto/views.py
@@ -21,7 +21,7 @@ def result_lotto(request):
             lotto.append(random.randint(1, 45))
             lotto = list(set(lotto))
         
-        result_lotto.append(lotto)
+        result_lotto.append(sorted(lotto))
 
     # 정렬 및 응답
-    return render(request, 'lotto_result.html', {'num_of_game':num_of_game,'result':sorted(result_lotto)})
+    return render(request, 'lotto_result.html', {'num_of_game':num_of_game,'result':result_lotto})
